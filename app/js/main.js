efd06708b2aae75b970461f703dfcb521b1a590b
Alexander Bochko <mr.bochko@gmail.com>
2015-08-01 22:10:52 +0300

    Добавлено модальное окно

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,34 @@
-$(function()
-	{console.log('Файл main.js успешно загружен')
-});
+var myModule = (function () {
+
+    var init = function () {
+
+         _setUpListners();
+
+        };
+
+    var _setUpListners = function (){
+
+            $('#add-new-item').on('click', _showModal);
+
+        };
+
+    var _showModal = function (ev){
+
+        console.log('Вызов модального окна')
+
+        ev.preventDefault();
+        $('#add-project-popup').bPopup({
+            speed: 650,
+            transition: 'slideDown'});
+
+
+        };
+
+    return {
+        init: init
+    };
+
+})();
+
+myModule.init();
+
